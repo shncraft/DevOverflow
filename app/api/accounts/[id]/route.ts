a133513c4503d1d3a30d90736b23cc0a1e0b76e4
Shani Sharma <shanisharrma13@gmail.com>
2026-06-22 17:58:56 +0530

    Implement Dynamic Accounts Routes

---
+++ app/api/accounts/[id]/route.ts
@@ -0,0 +1,78 @@
+import { Account } from "@/database";
+import handleError from "@/lib/handlers/error";
+import { NotFoundError, ValidationError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
+import { AccountSchema } from "@/lib/validations";
+import { APIErrorResponse } from "@/types/global";
+import { NextRequest, NextResponse } from "next/server";
+
+// GET /api/accounts/{id}
+export async function GET(
+  _: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  const { id } = await params;
+  if (!id) throw new NotFoundError("Account");
+
+  try {
+    await dbConnect();
+
+    const account = await Account.findById(id);
+    if (!account) throw new NotFoundError("Account");
+
+    return NextResponse.json({ success: true, data: account }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+// PUT /api/account/{id}
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  const { id } = await params;
+  if (!id) throw new NotFoundError("Account");
+
+  try {
+    await dbConnect();
+
+    const body = await request.json();
+    const parsed = AccountSchema.partial().safeParse(body);
+
+    if (!parsed.success)
+      throw new ValidationError(parsed.error.flatten().fieldErrors);
+
+    const updatedAccount = await Account.findByIdAndUpdate(id, parsed.data, {
+      new: true,
+    });
+    if (!updatedAccount) throw new NotFoundError("Account");
+
+    return NextResponse.json(
+      { success: true, data: updatedAccount },
+      { status: 200 },
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+// DELETE /api/accounts/{id}
+export async function DELETE(
+  _: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  const { id } = await params;
+  if (!id) throw new NotFoundError("Account");
+
+  try {
+    await dbConnect();
+
+    const account = await Account.findByIdAndDelete(id);
+    if (!account) throw new NotFoundError("Account");
+
+    return NextResponse.json({ success: true, data: account }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
