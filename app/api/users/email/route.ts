fec5600b835acefcc63be127dc40e06434622fef
Shani Sharma <shanisharrma13@gmail.com>
2026-06-22 17:39:47 +0530

    Implement Get User By Email Routes

---
+++ app/api/users/email/route.ts
@@ -0,0 +1,25 @@
+import { User } from "@/database";
+import handleError from "@/lib/handlers/error";
+import { NotFoundError, ValidationError } from "@/lib/http-errors";
+import { UserSchema } from "@/lib/validations";
+import { APIErrorResponse } from "@/types/global";
+import { NextRequest, NextResponse } from "next/server";
+
+// POST /api/users/email
+export async function POST(request: NextRequest) {
+  const { email } = await request.json();
+
+  try {
+    const parsed = UserSchema.partial().safeParse({ email });
+
+    if (!parsed.success)
+      throw new ValidationError(parsed.error.flatten().fieldErrors);
+
+    const user = await User.findOne({ email });
+    if (!user) throw new NotFoundError("User");
+
+    return NextResponse.json({ success: true, data: user }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
