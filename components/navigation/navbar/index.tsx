cb91b33ee341a8d07b09d84f34ff4ca0c86a9bf0
Shani Sharma <shanisharrma13@gmail.com>
2026-06-23 21:02:45 +0530

    Implement user avatar in navbar

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -1,8 +1,11 @@
 import Theme from "./theme";
 import { MobileNavigation } from "./mobile-navigation";
 import { AppLogo } from "./app-logo";
+import { auth } from "@/auth";
+import { UserAvatar } from "@/components/user-avatar";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await auth();
   return (
     <div className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 dark:shadow-none sm:px-12 ">
       <AppLogo TextClassName="max-sm:hidden" />
@@ -11,6 +14,14 @@ const Navbar = () => {
 
       <div className="flex-between gap-5">
         <Theme />
+
+        {session?.user && (
+          <UserAvatar
+            id={session.user.id!}
+            name={session.user.name!}
+            image={session.user.image}
+          />
+        )}
 
         <MobileNavigation />
       </div>
---
+++ components/user-avatar.tsx
@@ -0,0 +1,51 @@
+import ROUTES from "@/constants/routes";
+import Link from "next/link";
+import { Avatar, AvatarFallback } from "./ui/avatar";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
+interface UserAvatarProps {
+  id: string;
+  name: string;
+  image?: string | null;
+  className?: string;
+}
+
+export function UserAvatar({
+  id,
+  name,
+  image,
+  className = "h-9 w-9",
+}: UserAvatarProps) {
+  const initials = name
+    .split(" ")
+    .map((word) => word[0])
+    .join("")
+    .toUpperCase()
+    .slice(0, 2);
+  return (
+    <Link href={ROUTES.PROFILE(id)}>
+      <Avatar
+        className={cn(
+          className,
+          "rounded-full border-3 border-primary-500/50 hover:border-primary-500/80 transition-colors duration-200 ease-in-out",
+        )}
+      >
+        {image ? (
+          <Image
+            src={image}
+            alt={name}
+            className="object-cover rounded-full"
+            width={36}
+            height={36}
+            quality={100}
+          />
+        ) : (
+          <AvatarFallback className="primary-gradient font-space-grotesk font-bold tracking-wider text-white">
+            {initials}
+          </AvatarFallback>
+        )}
+      </Avatar>
+    </Link>
+  );
+}
