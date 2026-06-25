7cabdc39ad5c2451d62e2c0584306b8820fd725b
Shani Sharma <shanisharrma13@gmail.com>
2026-06-25 16:20:32 +0530

    Implement GetTags server action

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,3 +1,15 @@
-export default function TagsPage() {
+import { getTags } from "@/lib/actions/tag.action";
+
+export default async function TagsPage() {
+  const { success, data, error } = await getTags({
+    page: 1,
+    pageSize: 10,
+    query: "js",
+  });
+
+  const { tags } = data || {};
+
+  console.log("tags:", tags);
+
   return <div>TagsPage</div>;
 }
---
+++ lib/actions/tag.action.ts
@@ -0,0 +1,84 @@
+"use server";
+
+import { Tag } from "@/database";
+import action from "../handlers/action";
+import handleError from "../handlers/error";
+import { PaginatedSearchParamSchema } from "../validations";
+
+export async function getTags(
+  params: PaginatedSearchParams,
+): Promise<ActionResponse<{ tags: Tag[]; isNext: boolean }>> {
+  // validate the params
+  const validatedResult = await action({
+    params,
+    schema: PaginatedSearchParamSchema,
+  });
+
+  // return error if not validated
+  if (validatedResult instanceof Error) {
+    return handleError(validatedResult) as ErrorResponse;
+  }
+
+  // prepare the limit and offset
+  const {
+    page = 1,
+    pageSize = 10,
+    filter,
+    query,
+    sort,
+  } = validatedResult.params!;
+  const limit = Number(pageSize);
+  const offset = (Number(page) - 1) * pageSize;
+
+  // Build filterQuery
+  const filterQuery: Record<string, unknown> = {};
+
+  if (query) {
+    filterQuery.$or = [
+      {
+        name: {
+          $regex: query,
+          $options: "i",
+        },
+      },
+    ];
+  }
+
+  // Build sort criteria
+  let sortCriteria = {};
+
+  switch (filter) {
+    case "popular":
+      sortCriteria = { questions: -1 };
+      break;
+    case "recent":
+      sortCriteria = { createdAt: -1 };
+      break;
+    case "oldest":
+      sortCriteria = { createdAt: 1 };
+      break;
+    case "name":
+      sortCriteria = { name: 1 };
+      break;
+    default:
+      sortCriteria = { questions: -1 };
+      break;
+  }
+
+  try {
+    // fetch total number of tags
+    const totalTags = await Tag.countDocuments(filterQuery);
+
+    // fetch all tags
+    const tags = await Tag.find(filterQuery)
+      .sort(sortCriteria)
+      .skip(offset)
+      .limit(limit);
+
+    const isNext = totalTags > offset + tags.length;
+
+    return { success: true, data: { tags, isNext } };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
