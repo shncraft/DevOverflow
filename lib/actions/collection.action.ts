de21bf02df70958a089a304f4d914852872272a3
Shani Sharma <shanisharrma13@gmail.com>
2026-06-30 16:52:59 +0530

    Implement collection save action

---
+++ lib/actions/collection.action.ts
@@ -0,0 +1,53 @@
+"use server";
+
+import { Collection, Question } from "@/database";
+import action from "../handlers/action";
+import handleError from "../handlers/error";
+import { CollectionBaseSchema } from "../validations";
+import { NotFoundError } from "../http-errors";
+import { revalidatePath } from "next/cache";
+import ROUTES from "@/constants/routes";
+
+export async function toggleSaveQuestionAction(
+  params: CollectionBaseParams,
+): Promise<ActionResponse<{ saved: boolean }>> {
+  const validatedResult = await action({
+    params,
+    schema: CollectionBaseSchema,
+    authorize: true,
+  });
+
+  if (validatedResult instanceof Error) {
+    return handleError(validatedResult) as ErrorResponse;
+  }
+
+  const { questionId } = validatedResult.params!;
+  const userId = validatedResult.session?.user?.id;
+
+  try {
+    const question = await Question.findById(questionId);
+    if (!question) {
+      throw new NotFoundError("Question");
+    }
+
+    const collection = await Collection.findOne({
+      question: questionId,
+      author: userId,
+    });
+    if (collection) {
+      await Collection.findByIdAndDelete(collection.id);
+      return {
+        success: true,
+        data: { saved: false },
+      };
+    }
+
+    await Collection.create({ question: questionId, author: userId });
+
+    revalidatePath(ROUTES.QUESTION(questionId));
+
+    return { success: true, data: { saved: false } };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
--- lib/validations.ts
+++ lib/validations.ts
@@ -159,3 +159,7 @@ export const HasVotedSchema = CreateVoteSchema.pick({
   targetId: true,
   targetType: true,
 });
+
+export const CollectionBaseSchema = z.object({
+  questionId: z.string().min(1, "Question ID is required."),
+});
